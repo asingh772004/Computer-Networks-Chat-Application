08e51b8f6f6951758947bb843062e5edb39fe625
MEYSAKTHI <155127080+MEYSAKTHI@users.noreply.github.com>
2025-02-14 18:04:51 +0000

    BASIC CLIENT

--- client.cpp
+++ client.cpp
@@ -5,3 +5,96 @@
 #include <netinet/in.h>
 #include <pthread.h>
 #include <csignal>
+
+#define RED "\033[31m"          //red
+#define RESET "\033[0m"         
+#define YELLOW "\033[33m"       // yellow
+#define RED     "\033[31m"      // Red color
+#define GREEN   "\033[32m"      // Green color
+#define WHITE   "\033[37m"      // White color
+
+int sockfd;
+
+void error(const char *msg) {
+    perror(msg);
+    exit(1);
+}
+
+void* receive_messages(void* arg) {
+    char buffer[256];
+    while (true) {
+        bzero(buffer, 256);
+        ssize_t n = read(sockfd, buffer, 255);
+        if (n < 0) {
+            error("ERROR reading from socket");
+        }
+        receive_handler(buffer);
+    }
+    return nullptr;
+}
+
+receive_handler(char *buffer)
+{
+    string message(buffer);
+
+    if(message[0]=="B")
+    {
+        cout<<GREEN<<" "<<message.substr(1)<< " "<<RESET<<endl;
+    }
+    else
+    {
+        cout<<GREEN<<" "<<message.substr(1)<< " "<<RESET<<endl;
+    }
+}
+
+void* send_messages(void* arg) {
+    char buffer[256];
+    while (true) {
+        bzero(buffer, 256);
+        fgets(buffer, 255, stdin);
+        printf(YELLOW " : YOU \n" RESET);
+        ssize_t n = write(sockfd, buffer, strlen(buffer));
+        if (n < 0) {
+            error("ERROR writing to socket");
+        }
+    }
+    return nullptr;
+}
+
+int main(int argc, char *argv[]) {
+    int portno;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    if (argc < 3) {
+        fprintf(stderr, "usage %s hostname port\n", argv[0]);
+        exit(1);
+    }
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        error("ERROR opening socket");
+    }
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr, "ERROR, no such host\n");
+        exit(1);
+    }
+    bzero((char*)&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char*)server->h_addr, (char*)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        error("ERROR connecting");
+    }
+
+    pthread_t send_thread, receive_thread;
+    pthread_create(&send_thread, nullptr, send_messages, nullptr);
+    pthread_create(&receive_thread, nullptr, receive_messages, nullptr);
+
+    pthread_join(send_thread, nullptr);
+    pthread_join(receive_thread, nullptr);
+
+    close(sockfd);
+    return 0;
+}
