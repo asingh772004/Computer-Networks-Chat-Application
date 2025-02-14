f01cfb02b9f811814282999d079f85319369ce90
MEYSAKTHI <155127080+MEYSAKTHI@users.noreply.github.com>
2025-02-14 18:32:30 +0000

    fixed compilation errors

--- client.cpp
+++ client.cpp
@@ -5,15 +5,18 @@
 #include <netinet/in.h>
 #include <pthread.h>
 #include <csignal>
+#include <netdb.h>  // Include this for gethostbyname()
 
-#define RED "\033[31m"          //red
+using namespace std;
+
+#define RED "\033[31m"          // Red
 #define RESET "\033[0m"         
-#define YELLOW "\033[33m"       // yellow
-#define RED     "\033[31m"      // Red color
-#define GREEN   "\033[32m"      // Green color
-#define WHITE   "\033[37m"      // White color
+#define YELLOW "\033[33m"       // Yellow
+#define GREEN "\033[32m"        // Green
+#define WHITE "\033[37m"        // White
 
 int sockfd;
+void receive_handler(char *buffer) ;
 
 void error(const char *msg) {
     perror(msg);
@@ -23,7 +26,7 @@ void error(const char *msg) {
 void* receive_messages(void* arg) {
     char buffer[256];
     while (true) {
-        bzero(buffer, 256);
+        memset(buffer, 0, sizeof(buffer));  // Use memset instead of bzero
         ssize_t n = read(sockfd, buffer, 255);
         if (n < 0) {
             error("ERROR reading from socket");
@@ -33,26 +36,22 @@ void* receive_messages(void* arg) {
     return nullptr;
 }
 
-receive_handler(char *buffer)
-{
-    string message(buffer);
+void receive_handler(char *buffer) {
+    string message(buffer);  
 
-    if(message[0]=="B")
-    {
-        cout<<GREEN<<" "<<message.substr(1)<< " "<<RESET<<endl;
-    }
-    else
-    {
-        cout<<RED<<" "<<message.substr(1)<< " "<<RESET<<endl;
+    if (message[0] == 'B') {  
+       cout << GREEN << " " << message.substr(1) << " " << RESET <<endl;
+    } else {
+       cout << RED << " " << message.substr(1) << " " << RESET << endl;
     }
 }
 
 void* send_messages(void* arg) {
     char buffer[256];
     while (true) {
-        bzero(buffer, 256);
+        memset(buffer, 0, sizeof(buffer));  // Use memset instead of bzero
         fgets(buffer, 255, stdin);
-        printf(YELLOW " : YOU \n" RESET);
+        cout << YELLOW << " : YOU \n" << RESET;
         ssize_t n = write(sockfd, buffer, strlen(buffer));
         if (n < 0) {
             error("ERROR writing to socket");
@@ -70,20 +69,24 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "usage %s hostname port\n", argv[0]);
         exit(1);
     }
+    
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
         error("ERROR opening socket");
     }
+
     server = gethostbyname(argv[1]);
     if (server == NULL) {
         fprintf(stderr, "ERROR, no such host\n");
         exit(1);
     }
-    bzero((char*)&serv_addr, sizeof(serv_addr));
+
+    memset((char*)&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy((char*)server->h_addr, (char*)&serv_addr.sin_addr.s_addr, server->h_length);
     serv_addr.sin_port = htons(portno);
+
     if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
         error("ERROR connecting");
     }
