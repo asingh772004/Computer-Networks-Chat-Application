b7db634ba2042a798cd4614f957606e34d4743e3
Kumar Snehal <135157197+Kumar-Snehal@users.noreply.github.com>
2025-02-14 19:11:41 +0000

    added buffer

--- client.cpp
+++ client.cpp
@@ -25,6 +25,21 @@ void error(const char *msg)
     exit(1);
 }
 
+void *receive_messages(void *arg)
+{
+    char buffer[256];
+    while (true)
+    {
+        bzero(buffer, 256);
+        ssize_t n = read(sockfd, buffer, 255);
+        if (n < 0)
+        {
+            error("ERROR reading from socket");
+        }
+        receive_handler(buffer);
+    }
+    return nullptr;
+}
 
 void receive_handler(char *buffer)
 {
@@ -40,12 +55,30 @@ void receive_handler(char *buffer)
     }
 }
 
+void *send_messages(void *arg)
+{
+    char buffer[256];
+    while (true)
+    {
+        bzero(buffer, 256);
+        fgets(buffer, 255, stdin);
+        cout << YELLOW << " : YOU \n"
+             << RESET;
+        ssize_t n = write(sockfd, buffer, strlen(buffer));
+        if (n < 0)
+        {
+            error("ERROR writing to socket");
+        }
+    }
+    return nullptr;
+}
 
 int main(int argc, char *argv[])
 {
     int portno;
     struct sockaddr_in serv_addr;
     struct hostent *server;
+    char buffer[256];
 
     if (argc < 3)
     {
@@ -74,43 +107,14 @@ int main(int argc, char *argv[])
     {
         error("ERROR connecting");
     }
-    
-      bzero(buffer, 256);
-        ssize_t n = read(sockfd, buffer, 255);
-        if (n < 0)
-        {
-            error("ERROR reading from socket");
-        }
-        receive_handler(buffer);
-         bzero(buffer, 256);
-        fgets(buffer, 255, stdin);
-        cout << YELLOW << " : YOU \n"
-             << RESET;
-        n = write(sockfd, buffer, strlen(buffer));
-        if (n < 0)
-        {
-            error("ERROR writing to socket");
-        }  
-        
-        
-        while(true)
-        {
-           bzero(buffer, 256);
-      	   fgets(buffer, 255, stdin);
-           cout << YELLOW << " : YOU \n"<< RESET;
-           n = write(sockfd, buffer, strlen(buffer));
-           if (n < 0)
-      	   {
-            error("ERROR writing to socket");
-       	   }  
-       	    bzero(buffer, 256);
-            n = read(sockfd, buffer, 255);
-      	  if (n < 0)
-      	  {
-            error("ERROR reading from socket");
-      	  }
-        receive_handler(buffer);
-        }     
+
+    pthread_t send_thread, receive_thread;
+    pthread_create(&send_thread, nullptr, send_messages, nullptr);
+    pthread_create(&receive_thread, nullptr, receive_messages, nullptr);
+
+    pthread_join(send_thread, nullptr);
+    pthread_join(receive_thread, nullptr);
+
     close(sockfd);
     return 0;
 }
